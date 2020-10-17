ca681f4e3f82f70444531fff7a9635cebf1ae262
daph87 <52703134+daph87@users.noreply.github.com>
2020-10-17 18:29:47 +0300

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,7 @@ const About = () => {
           <h1>About this app</h1>  
           <p>App to search github users</p>
           <p>Version : 1.0.0</p>
+          <p>Version : 1.0.0</p>
         </Fragment>
     )
 }
